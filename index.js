0e345d8e2ec6e8a566f309ee0610cdd0bbbd27d1
Vinayak Tiwari <84216407+VinayakTiwari1103@users.noreply.github.com>
2025-01-10 00:17:01 +0530

    setting up my devops community index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set('view engine' , 'ejs')
+app.set('views' , path.resolve("./views") )
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+
