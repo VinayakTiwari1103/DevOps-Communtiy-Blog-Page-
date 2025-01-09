fdb635a35b47585e97b0cf9bb47168f9f8966c92
Vinayak Tiwari <84216407+VinayakTiwari1103@users.noreply.github.com>
2025-01-10 01:33:13 +0530

    nav bar setup as well as user schema update

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const PORT = 8000;
 
 app.set('view engine' , 'ejs')
 app.set('views' , path.resolve("./views") )
+
+
+app.get('/', (req,res) =>{
+    res.render("home")
+})
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+
+
 
